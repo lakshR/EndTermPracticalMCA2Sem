ae5153b31c7a6e1941b04daf28863e11e88ec82c
pathaksuraj <86240225+pathaksuraj@users.noreply.github.com>
2021-08-06 11:15:27 +0530

    Add files via upload

---
+++ Answers MCA Sec C/SURAJ_PATHAK-2001154/KeyEvents.java
@@ -0,0 +1,38 @@
+//Name:suraj pathak
+//Program 2nd:-
+
+import java.awt.*;
+import java.applet.*;
+import java.awt.event.*;
+public class KeyEvents extends Applet implements KeyListener
+{
+    String msg = "";
+ 
+    public void init()
+    {
+        addKeyListener(this);
+    }
+ 
+    public void keyReleased(KeyEvent k)
+    {
+        showStatus("Key Released");
+        repaint();
+    }
+ 
+    public void keyTyped(KeyEvent k)
+    {
+        showStatus("Key Typed");
+        repaint();
+    }
+ 
+    public void keyPressed(KeyEvent k)
+    {
+        showStatus("Key Pressed");
+        repaint();
+    }
+ 
+    public void paint(Graphics g)
+    {
+        g.drawString(msg, 10, 10);
+    }
+}
